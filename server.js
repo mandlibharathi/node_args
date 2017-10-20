8cf92cf2577dae29e9088d7dc350c7b64a548428
bharathi22 <bharathi22@gmail.com>
2017-10-20 13:35:10 +0530

    commeted server.js

--- server.js
+++ server.js
@@ -5,9 +5,5 @@ app.get('/', function (req, res) {
    res.send('Hello World');
 })
 
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
-})
+var server = app.listen(8081)
+console.log('server is runing');
