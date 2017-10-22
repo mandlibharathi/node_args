e2e37932e9b8037ca7e40842b3b8200ba856db9c
mandlibharathi <mandlibharathi@gmail.com>
2017-10-22 14:28:32 +0530

    modified server.js

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ var app = express();
 
 app.get('/', function (req, res) {
    res.send('Hello World');
-})
+});
+app.route('/node').get(function(req,res){
+res.send('welcome to nodejs');
+});
 
 var server = app.listen(8081)
 console.log('server is runing at http://localhost:8081');
