200043e39635614b11ede3ba6e1006a194e7d1af
mandlibharathi <mandlibharathi@gmail.com>
2017-10-31 14:13:11 +0530

    updeted

--- http.js
+++ http.js
@@ -1,4 +1,7 @@
 var http=require('http');
-http.createServer=function(req,res){
+http.createServer(function(req,res){
+console.log("res " + res.statusCode);
 res.end("hello world");
+console.log("req " +req.method);
 }).listen(8000);
+console.log("server is running");
