7e6a719f522515d43014e7bfa8855b62ff4ee653
mandlibharathi <mandlibharathi@gmail.com>
2017-11-08 21:30:24 +0530

    added the file

--- middleware.js
+++ middleware.js
@@ -1,13 +1,14 @@
 var express=require('express');
-var app=express();
-app.use('/',function(req,res,next){
-    console.log("the request is :" +req.method +'\n' +"andd"   + req.url);
+var bodyParser=require('body-parser');
 
-next()
-})
+var app=express();
+app.use(bodyParser.json({type:"application/json"}));
+var person={
+name:"xxxxx",
+age:19
+}
 app.get('/',function(req,res){
-    res.send("hello world")
+res.send(person);
 })
 app.listen(8000);
-console.log("here is some data")
-
+console.log("server is running");
