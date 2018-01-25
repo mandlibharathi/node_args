aa2be6bc6d3b45e4449c2331dd17659ed82dfc74
mandlibharathi <mandlibharathi@gmail.com>
2018-01-25 14:33:42 +0530

    server

---
+++ lists/server.js
@@ -0,0 +1,17 @@
+var express=require('express')
+var socket=require('socket.io')
+var app=express()
+var server=app.listen(3000,function(){
+    console.log('server is running...')
+})
+app.use(express.static('public'))
+var io=socket(server)
+io.on('connection',function(socket){
+    console.log('socket connected',socket.id)
+    socket.on('chat',function(data){
+        io.sockets.emit('chat',data)
+    })
+    socket.on('typing',function(data){
+        socket.broadcast.emit('typing',data)
+    })
+})
