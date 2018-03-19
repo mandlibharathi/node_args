84b83e68ab04ad01d377a2e41d3f51ebb44932da
mandlibharathi <mandlibharathi@gmail.com>
2018-03-19 21:54:53 +0530

    final commit

--- HELLO/schema.js
+++ HELLO/schema.js
@@ -2,10 +2,16 @@ var mongoose=require('mongoose')
 var Schema=mongoose.Schema;
 var bookSchema=new Schema({
     title:String,
-    name:String,
-},
-    {
-        timpstamps:true
-    
+    description:String,
+    Author:String,
+    publish:String,
+    rating:String,
+    price:String,
+    votes:Number,
+    available_stores:{
+type:Array,
+"default":[]
+    }
 })
+
 module.exports=mongoose.model('book',bookSchema)
