903458a193aa9b1b51a1dc25befdae6e0bbb79f2
mandlibharathi <mandlibharathi@gmail.com>
2018-03-19 21:54:19 +0530

    router

--- HELLO/router.js
+++ HELLO/router.js
@@ -1,27 +1,48 @@
 var express=require('express');
 var router=express.Router()
 var book=require('./schema')
-router.get('/',(req,res)=>{
-    book.find((err,user)=>{
-        if(err) throw err
-            else{
-                res.send(user)
-            }
-    })
-
-})
 router.post('/',(req,res)=>{
-    var title=req.body.title;
-    var name=req.body.name;
-    var newbook=new book
+   var title=req.body.title;
+   var description=req.body.description
+   var Author=req.body.Author;
+   var  publish=req.body.publish;
+   var rating=req.body.ratng;
+   var votes=req.body.votes;
+   var price=req.body.price;
+   var available_stores=req.body.available_stores;
+   var newbook=new book()
     newbook.title=title;
-    newbook.name=name;
-    newbook.save((err,data)=>{
-if(err) throw err
-    else{
-        res.json(data)
+    newbook.description=description;
+    newbook.Author=Author;
+    newbook.publish=publish;
+    newbook.rating=rating;
+    newbook.price=price;
+    newbook.votes=votes;
+    newbook.available_stores=available_stores;
+
+book.findOne({'title':title},(err,user)=>{
+    if(err){
+        throw err;
     }
-    })
+    if(user){
+        res.json({msg:'title already exists'})
+        
+    }
+    else{
+        newbook.save((err,data)=>{
+            if(err)
+                throw err
+            res.send(data)
+        })
+    }
+})
+
+
+
+
+
+
+
 
 })
 module.exports=router;
